3cebe7744a0f632ba7bdcd398955ec37790607ee
yjb1248887529 <1248887529@qq.com>
2019-09-24 18:41:34 +0800

    Initial commit

---
+++ app/src/main/java/com/chouqu/MainActivity.java
@@ -0,0 +1,15 @@
+package com.chouqu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.chouqu.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/chouqu/base/BaseActivity.java
@@ -0,0 +1,7 @@
+package com.chouqu.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+}
